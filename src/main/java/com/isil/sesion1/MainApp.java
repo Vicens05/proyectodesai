dd8c10c708a1b829ec681d557f672d0be2eed607
Vicens05 <vicesimo55@gmail.com>
2022-09-07 10:44:56 -0500

    Semana 2 - Se agrega prepared

--- src/main/java/com/isil/sesion1/MainApp.java
+++ src/main/java/com/isil/sesion1/MainApp.java
@@ -1,17 +1,16 @@
 package com.isil.sesion1;
 
 import javax.xml.transform.Result;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MainApp {
 
     public static void main(String[] args) throws Exception{    //throws Exception para el error con forName
 
+        //1. Cargar driver
         Class.forName("com.mysql.cj.jdbc.Driver");     //para vincular con el mysql
 
+        //2. Crear conexión
         Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/TEST",        //TEST es el nombre de la base de datos
                 "root",
@@ -19,25 +18,43 @@ public class MainApp {
 
         );
 
-        Statement stmt= con.createStatement();
-        //int resultUpdate = stmt.executeUpdate("update Users set city='Barcelona' where name='Messi'");  //para actualizar el dato sin ir a la base de datos
+        //3. Crear statement
+        //3.1 Statement mantenimiento
+        /* INSERTAR
+        Statement stCreate = con.createStatement();
+        int filasAfectadas =
+                stCreate.executeUpdate("insert into Users values (6, 'Maria', '984784798','Quito')");*/
+        Statement stCreate = con.createStatement();
+        int filasAfectadas =
+                stCreate.executeUpdate("UPDATE USERS SET NAME='JUAN' WHERE idUser=1");
+        System.out.println("Filas afectadas: " + filasAfectadas);
 
-        ResultSet result = stmt.executeQuery("Select * FROM Users"); //mostrar resultado
-        //ResultSet result = stmt.executeQuery("Select * FROM Users where name='Messi'");
+        //3.2 Statement consulta
+        PreparedStatement preparedStatement =
+                con.prepareStatement("Select * from Users where idUser=?");
 
-        while(result.next()){
+        preparedStatement.setInt(1,1);
 
-            System.out.println(result.getString("idUser"));
-            System.out.println(result.getString("Name"));
-            System.out.println(result.getString("phone"));
-            System.out.println(result.getString("city"));
+        //PreparedStatement preparedStatement =
+                //con.prepareStatement("select * from Users where name =? and city=?");
+        //primer(1) parametro Cueva(x)
+        //preparedStatement.setString(1,"Cueva");
+       // preparedStatement.setString(2,"Lima");
 
-            /*
-            System.out.println(result.getString(1));
-            System.out.println(result.getString(2));
-            System.out.println(result.getString(3));
-            */
+        //4. Ejecutar query
+        //ResultSet resultSet2 = preparedStatement.executeQuery();
+        ResultSet resultSet2 = preparedStatement.executeQuery();
+
+
+        //5. Recorrer resultados
+        while(resultSet2.next()){
+            System.out.println(resultSet2.getString("name") +"\n"+
+                    resultSet2.getString("phone") +"\n"+
+                    resultSet2.getString("city"));
         }
+
+        //6. Cerrar conexion
+        con.close();
 
     }
 
